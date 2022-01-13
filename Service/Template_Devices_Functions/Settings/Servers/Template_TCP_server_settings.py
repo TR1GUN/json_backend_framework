2946f959fe2053932e779469f7da22c02218d440
TRIGUN-D <aka_gto@mail.ru>
2022-01-13 20:29:18 +0300

    - Сервера - переделываем TCP

--- Service/Template_Devices_Functions/Settings/Servers/Template_TCP_server_settings.py
+++ Service/Template_Devices_Functions/Settings/Servers/Template_TCP_server_settings.py
@@ -32,7 +32,7 @@ class TemplateServer_TCP(TemplateFunctional):
         :return:
         """
         # делаем запрос - получаем ответ
-        response = self._request_GET(JSON='')
+        response = self._request_GET()
 
         return response
 
