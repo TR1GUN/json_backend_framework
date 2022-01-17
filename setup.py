dcb63fa49ec23477ad24b884fe7c275daef273db
TRIGUN-D <aka_gto@mail.ru>
2022-01-17 15:54:39 +0300

    -пытаемся все это запихнуть в пакет -2

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
 
-def setup():
-    """
+# def setup():
+#     """
+#
+#     """
+#
+#     print("UM-40 SMART Framework")
 
-    """
+import setuptools
 
-    print("UM-40 SMART Framework")
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
 
+setuptools.setup(
+    name="JSON_Backend_Framework",
+    version="0.1",
+    author="Buslin Nicolay",
+    author_email="n.Buslin@allmonitoring.ru",
+    description="Package to eaZZZy work with USPD.",
+    # long_description=long_description, <<---README.md
+    packages=setuptools.find_packages(),
+
+
+    install_requires=[
+        'requests==2.25.1',
+    ]
+)
